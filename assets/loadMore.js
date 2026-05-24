12ad056f6307d8f6631d1013b738fe613cb451b4
Globalvisionpoint <contact@globalvisionpoint.com>
2026-05-24 20:53:11 +0300

    Make featured collection load-more button toggle expand/collapse

--- assets/loadMore.js
+++ assets/loadMore.js
@@ -2,49 +2,97 @@ class loadMoreProduct extends HTMLElement {
   constructor() {
     super();
     this.addEventListener("click", this.onClickLoadMore);
+    this.initialProductsHTML = null;
+    this.initialButtonHTML = null;
   }
-  onClickLoadMore = async (evt) => {
-    let loadMoreBtnTarget = evt.target.classList.contains('loadMoreBtn');
-    if(loadMoreBtnTarget){
-      let loadMoreBtn = evt.target;
-      let currentPage = loadMoreBtn.dataset.currentPage;
-      let collectionUrl = loadMoreBtn.dataset.url;
-      let SectionId = loadMoreBtn.dataset.sectionId;
-      currentPage = parseInt(currentPage);
 
-      let productWrapper = this.querySelector(".collection__product");
-      let loadMoreBtnWrapper = this.querySelector(".loadMoreWrapper");
-      loadMoreBtn.classList.add("loading");
-      
-      try {
+  setButtonState = (button, expanded) => {
+    if (!button) return;
+
+    const labelNode = button.querySelector(".loadMoreBtn__label");
+    const iconNode = button.querySelector(".loadMoreBtn__icon");
+    const expandLabel = button.dataset.labelExpand || "Vezi toate produsele";
+    const collapseLabel = button.dataset.labelCollapse || "Restrange lista";
+
+    button.dataset.state = expanded ? "expanded" : "collapsed";
+    button.classList.toggle("is-expanded", expanded);
+
+    if (labelNode) {
+      labelNode.textContent = expanded ? collapseLabel : expandLabel;
+    }
+    if (iconNode) {
+      iconNode.textContent = expanded ? "▴" : "▾";
+    }
+  };
+
+  onClickLoadMore = async (evt) => {
+    const loadMoreBtn = evt.target.closest(".loadMoreBtn");
+    if (!loadMoreBtn || !this.contains(loadMoreBtn)) return;
+
+    evt.preventDefault();
+
+    const productWrapper = this.querySelector(".collection__product");
+    const loadMoreBtnWrapper = this.querySelector(".loadMoreWrapper");
+
+    if (!productWrapper || !loadMoreBtnWrapper) return;
+
+    if (loadMoreBtn.dataset.state === "expanded") {
+      if (this.initialProductsHTML) {
+        productWrapper.innerHTML = this.initialProductsHTML;
+      }
+      if (this.initialButtonHTML) {
+        loadMoreBtnWrapper.innerHTML = this.initialButtonHTML;
+      }
+      return;
+    }
+
+    if (!this.initialProductsHTML) {
+      this.initialProductsHTML = productWrapper.innerHTML;
+    }
+    if (!this.initialButtonHTML) {
+      this.initialButtonHTML = loadMoreBtnWrapper.innerHTML;
+    }
+
+    const collectionUrl = loadMoreBtn.dataset.url;
+    const sectionId = loadMoreBtn.dataset.sectionId;
+    let currentPage = parseInt(loadMoreBtn.dataset.currentPage, 10);
+
+    if (!collectionUrl || !sectionId || Number.isNaN(currentPage)) return;
+
+    loadMoreBtn.classList.add("loading");
+
+    try {
+      let hasMore = true;
+      while (hasMore) {
+        const nextPage = currentPage + 1;
         const response = await fetch(
-          `${collectionUrl}?page=${currentPage + 1}&section_id=${SectionId}`
+          `${collectionUrl}?page=${nextPage}&section_id=${sectionId}`
         );
         const resText = await response.text();
 
-        let lodedProductWrapper = new DOMParser().parseFromString(
-          resText,
-          "text/html"
-        );
-        let lodedProduct = lodedProductWrapper.querySelector(
-          ".collection__product"
-        ).innerHTML;
-        productWrapper.insertAdjacentHTML("beforeend", lodedProduct);
+        const loadedDoc = new DOMParser().parseFromString(resText, "text/html");
+        const loadedProducts = loadedDoc.querySelector(".collection__product");
+        if (!loadedProducts) break;
 
-        let loadedBtn = "";
-        if (lodedProductWrapper.querySelector(".loadMoreWrapper")) {
-          loadedBtn =
-            lodedProductWrapper.querySelector(".loadMoreWrapper").innerHTML;
+        productWrapper.insertAdjacentHTML("beforeend", loadedProducts.innerHTML);
+
+        const nextButton = loadedDoc.querySelector(".loadMoreWrapper .loadMoreBtn");
+        if (nextButton) {
+          currentPage = parseInt(nextButton.dataset.currentPage, 10);
+          if (Number.isNaN(currentPage)) {
+            hasMore = false;
+          }
+        } else {
+          hasMore = false;
         }
-        loadMoreBtnWrapper.innerHTML = loadedBtn;
-      } catch (error) {
-        console.log("error", error);
-      } finally {
-        loadMoreBtn.classList.remove("loading");
       }
 
+      this.setButtonState(loadMoreBtn, true);
+    } catch (error) {
+      console.log("error", error);
+    } finally {
+      loadMoreBtn.classList.remove("loading");
     }
-    
   };
 }
 customElements.define("load-more-product", loadMoreProduct);
